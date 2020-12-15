23cd3d7831aa4f6c40bdb2cc6a37d58648d42955
RifqiFirmasyah <56532181+UB8051@users.noreply.github.com>
2020-12-15 11:01:24 +0700

    Add project files.

---
+++ WebApplication/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication._Default" %>
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <br />
+    <div style="background-color: #800000; color: #FFFFFF; font-size: xx-large; text-align: center;">
+        <asp:Label ID="Label3" runat="server" Text="User Registration Form"></asp:Label>
+    </div>
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="User ID"></asp:Label>
+    <asp:TextBox ID="TextBoxID" runat="server" Width="162px"></asp:TextBox>
+    <br />
+    <br />
+
+
+    <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server" Width="171px"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+
+
+
+    <asp:TextBox ID="TextBox2" runat="server" Width="171px"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Search" />
+    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" />
+    <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Delete" />
+    <br />
+    <br />
+    <asp:Label ID="lblmsg" runat="server" Text="Label" Font-Bold="True"></asp:Label>
+
+
+
+    <br />
+
+
+
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+
+
+
+</asp:Content>
+
---
+++ WebApplication/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication.ServiceReference1;
+
+namespace WebApplication
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            InsertUser u = new InsertUser();
+
+            u.Name = TextBox1.Text;
+            u.Email = TextBox2.Text;
+
+            string r = client.Insert(u);
+
+            lblmsg.Text = r.ToString();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            gettstdata g = new gettstdata();
+            g = client.GetInfo();
+            DataTable dt = new DataTable();
+            dt = g.usertab;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            UpdateUser u = new UpdateUser();
+            u.UID = Convert.ToInt32(TextBoxID.Text);
+            u.Name = TextBox1.Text;
+            u.Email = TextBox2.Text;
+
+            string result = client.Update(u);
+            lblmsg.Text = result.ToString();
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DeleteUser u = new DeleteUser();
+            u.UID = Convert.ToInt32(TextBoxID.Text);
+
+
+            string result = client.Delete(u);
+            lblmsg.Text = result.ToString();
+        }
+    }
+}
